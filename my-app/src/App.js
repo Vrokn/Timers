3ae18fe81947deea9c6b61c695024d1c90fd32e4
Vrokn <61293690+Vrokn@users.noreply.github.com>
2020-07-22 12:23:35 -0500

    installed and tried framework semantic UI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button, Icon, } from 'semantic-ui-react';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <br></br>
+      <h1 class="ui dividing centered header">Timers</h1>
+      <div class="ui basic content center aligned segment">
+        <Button>
+          <Icon fitted name='plus' />
+        </Button>
+      </div>
+    </div >
   );
 }
 
